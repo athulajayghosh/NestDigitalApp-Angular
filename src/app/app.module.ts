3b6457cb47badcc1434371c11b98f073393a9bc4
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-11-30 15:09:04 +0530

    main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Homepage1Component } from './homepage1/homepage1.component';
+import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
+import { HomeProductsComponent } from './home-products/home-products.component';
+import { HomeAboutComponent } from './home-about/home-about.component';
+import { HometeamComponent } from './hometeam/hometeam.component';
+
+const myRoute:Routes=[
+  {path:"",
+  component:Homepage1Component},
+  {path:"products",
+  component:HomeProductsComponent},
+  {path:"about",
+  component:HomeAboutComponent},
+  {path:"team",
+  component:HometeamComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Homepage1Component,
+    HomeNavbarComponent,
+    HomeProductsComponent,
+    HomeAboutComponent,
+    HometeamComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hometeam/hometeam.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hometeam',
+  templateUrl: './hometeam.component.html',
+  styleUrls: ['./hometeam.component.css']
+})
+export class HometeamComponent {
+
+}
